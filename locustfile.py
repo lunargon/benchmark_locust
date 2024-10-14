80776aef0458fcabf4fc7909c927567a104b0755
ntluan <luan.nt@atomsolution.com.vn>
2024-10-14 13:49:28 +0700

    update locust file

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,10 @@
 from locust import HttpUser, between, task
 
-db_data ="odoo_benchmark"
-access_token = "eaP4hkinro73smB9kqw9AqAMXckns6"
+db_data = "odoo_benchmark"
+access_token = "zPsVlmeZC2eQpX12hbDbeWd5NcXZ77"
+
+
+# Menu test
 class MenuTest(HttpUser):
     wait_time = between(1, 15)
     host = "https://benchmark.vitrust.app"
@@ -32,36 +35,80 @@ class MenuTest(HttpUser):
             headers=header,
         )
 
-    # Get tuition
-    # @task
-    # def get_tuition(self):
-    #     header = {
-    #         "Accept": "application/json",
-    #         "Content-Type": "application/json",
-    #         "Authorization": f"Bearer {access_token}",
-    #     }
-    #     self.client.post(
-    #         f"/api/v2/call?db={db_data}",
-    #         json={
-    #             "model": "vt.tuition.program",
-    #             "method": "web_search_read",
-    #             "ids": [],
-    #             "kwargs": {
-    #                 "specification": {
-    #                     "code": {},
-    #                     "name": {},
-    #                     "description": {},
-    #                     "start_date": {},
-    #                     "due_date": {},
-    #                     "is_active": {},
-    #                     "res_partner_bank_id": {"fields": {"display_name": {}}},
-    #                 },
-    #                 "offset": 0,
-    #                 "order": "",
-    #                 "limit": 80,
-    #                 "count_limit": 10001,
-    #                 "domain": [],
-    #             },
-    #         },
-    #         headers=header,
-    #     )
+
+# Student test
+class StudentTest(HttpUser):
+    wait_time = between(1, 15)
+    host = "https://benchmark.vitrust.app"
+
+    # Get List Student
+    @task
+    def get_list_student(self):
+        header = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}",
+        }
+        self.client.post(
+            f"/api/v2/call?db={db_data}",
+            json={
+                "model": "res.partner",
+                "method": "web_search_read",
+                "ids": [],
+                "kwargs": {
+                    "specification": {
+                        "id": {},
+                        "name": {},
+                        "standard": {"fields": {"display_name": {}}},
+                        "div": {"fields": {"display_name": {}}},
+                        "curr_year": {"fields": {"display_name": {}}},
+                        "mobile": {},
+                        "email": {},
+                        "image_1920": {},
+                        "write_date": {},
+                    },
+                    "offset": 0,
+                    "order": "",
+                    "limit": 80,
+                    "count_limit": 10001,
+                    "domain": [["is_student", "=", True]],
+                },
+            },
+            headers=header,
+        )
+
+    # Get detail Student
+    @task
+    def get_detail_student(self):
+        header = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}",
+        }
+        self.client.post(
+            f"/api/v2/call?db={db_data}",
+            json={
+                "model": "res.partner",
+                "method": "web_read",
+                "ids": [7],
+                "kwargs": {
+                    "specification": {
+                        "is_student": {},
+                        "standard": {},
+                        "curr_year": {"fields": {"display_name": {}}},
+                        "stud_id": {},
+                        "gender": {},
+                        "surname": {},
+                        "caste": {},
+                        "vat": {},
+                        "state_id": {"fields": {"display_name": {}}},
+                        "district_id": {"fields": {"display_name": {}}},
+                        "ward_id": {"fields": {"display_name": {}}},
+                        "phone": {},
+                        "name": {},
+                        "street": {},
+                    }
+                },
+            },
+            headers=header,
+        )
